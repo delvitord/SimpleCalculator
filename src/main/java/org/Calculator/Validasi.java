b695dbe56c5d4f031931c043472570b7f85fc11b
delvitord <vitorahim@gmail.com>
2024-04-07 19:38:19 +0700

    Edit isNumeric

--- src/main/java/org/Calculator/Validasi.java
+++ src/main/java/org/Calculator/Validasi.java
@@ -4,14 +4,16 @@ public class Validasi {
     public static boolean isNumeric(String str) {
         if (str == null) {
             return false;
+        } else {
+            try {
+                double d = Double.parseDouble(str);
+            } catch (NumberFormatException nfe) {
+                return false;
+            }
+            return true;
         }
-        try {
-            double d = Double.parseDouble(str);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
     }
+
 
     public static boolean isValidOperator(String operator) {
         if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")){
